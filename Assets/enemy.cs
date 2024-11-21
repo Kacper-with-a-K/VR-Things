1fc975bedc08396189ddc6d4a7209fac2887a5c9
Kacper-with-a-K <58475524+superkacper27@users.noreply.github.com>
2024-11-21 09:00:13 +0000

    enemies working

--- Assets/enemy.cs
+++ Assets/enemy.cs
@@ -9,11 +9,43 @@ public class enemy : MonoBehaviour
     public float speed = 1;
     public float rotationSpeed = 5f;
 
+    public bool spotted;
+
+    public GameObject target;
+
+    public GameObject RespawnTips;
+
+    public GameObject respawnTarget;
     Animator anim;
 
     void Start()
     {
         anim = GetComponent<Animator>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == target)
+        {
+            spotted = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == target)
+        {
+            spotted = false;
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject == target)
+        {
+            RespawnTips.SetActive(true);
+            target.transform.position = respawnTarget.transform.position;
+        }
     }
 
     void Update()
@@ -22,6 +54,9 @@ public class enemy : MonoBehaviour
         {
             anim.SetBool("walk", true);
         }
+
+        if(spotted == false)
+        {
 
         GameObject waypoint = pattern[patternIndex];
 
@@ -55,5 +90,24 @@ public class enemy : MonoBehaviour
 
         Vector3 delta = speedDelta * normalizedRangeToClose;
         transform.Translate(delta, Space.World);
+        }
+
+        else if (spotted == true)
+        {
+            Vector3 rangeToClose = target.transform.position - transform.position;
+
+            float distance = rangeToClose.magnitude;
+            float speedDelta = speed * Time.deltaTime;
+            Vector3 normalizedRangeToClose = rangeToClose.normalized;
+
+            if (normalizedRangeToClose != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(normalizedRangeToClose) * Quaternion.Euler(0, 180, 0);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+        Vector3 delta = speedDelta * normalizedRangeToClose;
+        transform.Translate(delta, Space.World);
+
+        }
     }
 }
